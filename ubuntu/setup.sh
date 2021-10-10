54a43624f91f174480c1a65324c425a831038704
marcusljx <marcusljx@gmail.com>
2021-10-10 14:30:00 +0800

    update ubuntu script

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -13,15 +13,23 @@ echo Installing apt packages: ${PACKAGES}
 # apts
 sudo apt update -yy && sudo apt upgrade -yy
 sudo apt install ${PACKAGES} -yy -q
+sudo apt autoremove -yy
 
-SNAP_PACKAGES="
-  curl\
+SNAP_PACKAGES=(
+  curl
+  docker
   goland
-"
+)
 
 # snaps
-sudo snap install --classic ${SNAP_PACKAGES}
+for package in ${SNAP_PACKAGES[*]}; do
+  echo [snap] Installing ${package}
+  sudo snap install --classic ${package}
+done
   
 # setup oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [[ -z "${ZSH}" ]]; then 
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
 
